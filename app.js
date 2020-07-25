ed1ab85187a372d2e9b506a0e0329113cdf0cc76
Veloriat <veloriat@gmail.com>
2020-07-25 15:59:01 +0800

    New file

---
+++ app.js
@@ -0,0 +1,46 @@
+// Minification
+
+(function ()
+{
+  'use strict';
+
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope', '$filter'];
+
+  function LunchCheckController($scope, $filter)
+  {
+
+    $scope.message = "";
+    $scope.list = "";
+    $scope.consider = "We do not consider empty item such as (, ,)";
+    var re = /\s*(?:,|$)\s*/;
+    var foodlist = $scope.list.split(re);
+    // var food.length = 0;
+    var food = [];
+    console.log(food)
+
+    $scope.change = function () {
+
+      var listahan = $scope.list;
+      var food = listahan.split(re);
+      console.log(food)
+
+      $scope.check = function () {
+      if (food.length > 3){
+        $scope.message = "Too much";
+      }
+      else if (food.length <= 3 && food.length >= 1){
+        $scope.message = "Enjoy";
+      }
+      else if (food.length == 0){
+        $scope.message = "Input first";
+      }
+
+      };
+
+    };
+
+  }
+})();
